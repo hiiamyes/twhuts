e9722c4d513c4b9e2f78d87aa91ae32f38aa46ff
Yes <joshuayes@storysensecomputing.com>
2015-03-26 11:03:28 +0800

    init commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+
+
+var app = express();
+
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+//
+var port = Number(process.env.PORT || 8080);
+app.listen(port, function() {
+    console.log("Listening on " + port);
+});
