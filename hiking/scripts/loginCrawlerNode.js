5b515a87c625b8b58cba57fc42cec6f12a400645
Yes <joshuayes@storysensecomputing.com>
2015-04-19 19:17:47 +0800

    Complete basic crawling and also fulfill 'go next month' operation.

--- hiking/scripts/loginCrawlerNode.js
+++ hiking/scripts/loginCrawlerNode.js
@@ -4,7 +4,7 @@ phantom.create(function(ph) {
     var url = 'http://recreation.forest.gov.tw/personal/Personal_index.aspx';
     var urlHut = 'http://recreation.forest.gov.tw/askformonhouse/AskForPaperAddNew.aspx?mode=0&AskSID=&houseid=C';
     var urlJQ = 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js';
-    var isLoginFinished = false;
+    var countOnLoadFinished = 0;
     ph.createPage(function(page) {
         page.open(url, function(status) {
             console.log("login page opened: ", status);
@@ -15,20 +15,63 @@ phantom.create(function(ph) {
             });
 
             page.set('onLoadFinished', function(success) {
-                if (!isLoginFinished) {
-                    page.open(urlHut, function(status) {
+                console.log('onLoadFinished: ' + countOnLoadFinished);
+                switch (countOnLoadFinished) {
+                    case 0:
+                        page.open(urlHut, function(status) {
+                            page.includeJs(urlJQ, function() {
+                                page.evaluate(function() {
+                                    var huts = [];
+                                    $('[id=eventscalendar_Label1_0]').each(function(i) {
+                                        var day = $(this).parents().eq(1);
+                                        // var date = new Date(new Date().getFullYear() + '-' + new Date().getMonth() + 1 + '-' + parseInt(day.find('.dayNumber').text()));
+                                        var date = new Date();
+                                        var remaining = day.find('#eventscalendar_Label1_0').html().split('<br>')[1].split(':')[1];
+                                        var applying = day.find('#eventscalendar_Label1_0').html().split('<br>')[2].split(':')[1];
+                                        huts.push({
+                                            'date': date,
+                                            'remaining': remaining,
+                                            'applying': applying
+                                        });
+                                    });
+                                    return huts;
+                                }, function(huts) {
+                                    console.log(huts);
+                                    page.evaluate(function() {
+                                        document.getElementById('eventscalendar_ctl00_NextMonth').click();
+                                    });
+                                });
+                            });
+                        });
+                        break;
+                    case 2:
                         page.includeJs(urlJQ, function() {
                             page.evaluate(function() {
-                                return $('#eventscalendar_ctl00_label2').text();
-                            }, function(result) {
-                                console.log('result: ' + result);
+                                var huts = [];
+                                $('[id=eventscalendar_Label1_0]').each(function(i) {
+                                    var day = $(this).parents().eq(1);
+                                    var date = day.find('.dayNumber').text();
+                                    var remaining = day.find('#eventscalendar_Label1_0').html().split('<br>')[1].split(':')[1];
+                                    var applying = day.find('#eventscalendar_Label1_0').html().split('<br>')[2].split(':')[1];
+                                    huts.push({
+                                        'date': date,
+                                        'remaining': remaining,
+                                        'applying': applying
+                                    });
+                                    huts.push(date);
+                                });
+                                return huts;
+                            }, function(huts) {
+                                console.log(huts);
                                 ph.exit()
                             });
                         });
-                    });
+                        break;
                 }
-                isLoginFinished = true;
+                countOnLoadFinished++;
+
             });
+
         });
     });
 });
